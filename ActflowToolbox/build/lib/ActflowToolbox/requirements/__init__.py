de7f7d81d7638dc2b6b81bcbf4118b5660755cd3
gnz5 <gnzakk@gmail.com>
2020-07-13 16:27:27 -0400

    added functions to requirements package to verify file sizes and number of files of downloaded data

--- ActflowToolbox/build/lib/ActflowToolbox/requirements/__init__.py
+++ ActflowToolbox/build/lib/ActflowToolbox/requirements/__init__.py
@@ -70,3 +70,44 @@ def verify_requirements():
             download_requirements()
         else:
             print("Requirements satisfied")
+            
+# given a directory, returns a tuple containing the number of files 
+# and the sum of the sizes of all files in the directory free
+def get_size(start_path = '.'):
+    total_size = 0
+    num_files = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+                num_files += 1
+                
+    return (num_files, total_size)            
+            
+# checks the number of files and total size of the dependencies,
+# docs, examples, and network_definitions folders        
+def verify_folder_sizes():
+    actflow_dir = pkg_resources.resource_filename('ActflowToolbox', '/')
+    dirs_to_check = ['dependencies', 'docs', 'examples', 'network_definitions']
+
+    print(os.listdir(actflow_dir))
+
+    if not os.getcwd().endswith('ActflowToolbox'):
+        os.chdir(actflow_dir)
+
+    for directory in dirs_to_check:
+        num_files, folder_size = get_size(directory)
+        print(num_files, 'files,', folder_size, 'bytes')
+
+
+
+
+
+
+
+
+
+
+    
